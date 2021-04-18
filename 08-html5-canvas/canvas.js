0df62589113b004ec3adb5c61b06339517a4eeb5
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-04-18 10:28:08 +0100

    Improve line with colour and size change

--- 08-html5-canvas/canvas.js
+++ 08-html5-canvas/canvas.js
@@ -10,14 +10,21 @@ ctx.strokeStyle  = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 100;
+//ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
     if (!isDrawing) return; // stop the fn from running when they are not moused down
     //console.log(e);
+    // change colour
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    // change line width
+    
     ctx.beginPath();
     // start from
     ctx.moveTo(lastX, lastY);
@@ -28,6 +35,22 @@ function draw(e) {
     // lastX = e.offsetX;
     // lastY = e.offsetY;
     [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++; // change colour
+    if (hue >= 360) {
+        hue = 0;
+    }
+
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+    
 }
 
 canvas.addEventListener('mousedown', (e) => {
