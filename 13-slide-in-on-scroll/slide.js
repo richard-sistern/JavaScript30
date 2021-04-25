5ea21b6b285c070a3f25de3cea2818a892b42cb5
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-04-25 20:33:53 +0100

    Finish slide tutorial

--- 13-slide-in-on-scroll/slide.js
+++ 13-slide-in-on-scroll/slide.js
@@ -1,3 +1,4 @@
+// Reduce the number of times a function is run over a given time
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function () {
@@ -11,4 +12,28 @@ function debounce(func, wait = 20, immediate = true) {
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args);
     };
-}
+};
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide (e) {
+    // console.log(e);
+
+    sliderImages.forEach(sliderImage => {
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // console.log(slideInAt);
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        };
+    });
+};
+
+// Anything with 'scroll' probably requires debounce
+window.addEventListener('scroll', debounce(checkSlide));
+
