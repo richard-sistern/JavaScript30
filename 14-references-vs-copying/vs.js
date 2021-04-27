bc5957a5e7bba870b69c3bae1247fd324ce6ab8f
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-04-27 16:12:01 +0100

    Finish lesson 14

--- 14-references-vs-copying/vs.js
+++ 14-references-vs-copying/vs.js
@@ -1,14 +1,27 @@
 // start with strings, numbers and booleans
+let age = 100;
+let age2 = age;
+console.log(age, age2);
+age = 200;
+console.log(age, age2);
+
+let name = 'Wes';
+let name2 = name;
+console.log(name, name2);
+name = 'Wesley';
+console.log(name, name2);
 
 // Let's say we have an array
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
 // and we want to make a copy of it.
+const team = players;
+console.log(players, team);
 
 // You might think we can just do something like this:
-
+team[3] = 'Lux';
 // however what happens when we update that array?
-
+// it will always reference back...
 // now here is the problem!
 
 // oh no - we have edited the original array too!
@@ -16,13 +29,18 @@ const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
 // So, how do we fix this? We take a copy instead!
+const team2 = players.slice();
 
 // one way
 
 // or create a new array and concat the old one in
-
+const team3 = [].concat(players);
 // or use the new ES6 Spread
-
+const team4 = [...players];
+team4[3] = 'pew';
+console.log(team4);
+console.log(players);
+const team5 = Array.from(players);
 // now when we update it, the original one isn't changed
 
 // The same thing goes for objects, let's say we have a person object
@@ -34,9 +52,33 @@ const person = {
 };
 
     // and think we make a copy:
-
+    const captain = person;
+    captain.number = 99;
+    
     // how do we take a copy instead?
-
+    const cap2 = Object.assign({}, person, { number: 99});
+    console.log(cap2);
     // We will hopefully soon see the object ...spread
 
     // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+    const wes = {
+        name: 'Wes',
+        age: 100,
+        social: {
+            twitter: '@wesbos',
+            facebook: 'wesbox.developer'
+        }
+    }
+
+    console.log(wes);
+
+    const dev = Object.assign({}, wes);
+
+    console.log(dev.social);
+    dev.social.twitter = '@coolman';
+    console.log(dev.social); // social references back to...
+    console.log(wes.social); 
+
+    // object assignment only goes one level deep
+    // use a deep clone function or poor mans version:
+    const dev2 = JSON.parse(JSON.stringify(wes));
