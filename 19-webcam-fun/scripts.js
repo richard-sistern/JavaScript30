6e8d60983aa833d2c4a517ab92b9f073efd80baa
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-05-22 15:45:48 +0100

    Add canvas video and take photo

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -3,3 +3,46 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            //console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(err => {
+            console.error(`OH NO!`, err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto() {
+    // Play the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    // Take photo
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    //link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt=Handsome" />`
+    strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+// Once video can play, it emits an event called 'canplay'
+video.addEventListener('canplay', paintToCanvas);
