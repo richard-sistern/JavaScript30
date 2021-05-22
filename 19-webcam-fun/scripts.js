ff324ef907e4bdea11634c327672f4fcaf68de39
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2021-05-22 15:58:54 +0100

    Add RGB split and ghosting

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -26,8 +26,14 @@ function paintToCanvas() {
         ctx.drawImage(video, 0, 0, width, height);
         // take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
+        
         // mess with them
-        pixels = redEffect(pixels);
+        //pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+
+        // add ghosting
+        ctx.globalAlpha = 0.1;
+
         // put them back
         ctx.putImageData(pixels, 0, 0)
 
@@ -54,6 +60,15 @@ function redEffect(pixels) {
         pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
         pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
         pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // red
+        pixels.data[i + 500] = pixels.data[i + 1]; // green
+        pixels.data[i - 150] = pixels.data[i + 2]; //blue
     }
     return pixels;
 }
